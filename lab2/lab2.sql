a6284f813ce0171333d66d8020e98acb80ada6cb
Brandon Ching <bching9@gmail.com>
2024-02-05 14:11:14 -0700

    add lab2

---
+++ lab2/lab2.sql
@@ -0,0 +1,24 @@
+/*
+    CSCI 403 Lab 2: Make (1)
+    
+    Name:
+*/
+
+-- do not put SET SEARCH_PATH in this file
+-- add your statements after the appropriate Step item
+-- it's fine to add additional comments as well
+
+/* Step 1: Create the table */
+
+
+/* Step 2: Insert the data */
+
+
+/* Step 3: Fix errors */
+
+
+/* Step 4: Drop a class */
+
+
+/* Step 5: Plan ahead */
+
