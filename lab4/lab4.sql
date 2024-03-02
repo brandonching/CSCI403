eabdfd7572c1ed0a816e9765f25e2a9f1caea8f3
Brandon Ching <bching9@gmail.com>
2024-03-01 18:21:27 -0700

    complete lab4

--- lab4/lab4.sql
+++ lab4/lab4.sql
@@ -21,21 +21,40 @@ SET search_path = history;
 ****************************************************************************/
 /*
     1. Which pioneers (first name, last name) won Turing Awards before the
-    year 2000?  (2 columns, 2 rows)
+    year 2000? (2 columns, 2 rows)
 */
 
+SELECT first, last
+FROM pioneer
+WHERE id IN (
+    SELECT pioneer_id
+    FROM turing_award
+    WHERE year_awarded < 2000
+);
 
 /*
     2. Which pioneers (first name, last name) did not win a Turing Award? 
     (2 columns, 13 rows)
 */
 
+SELECT first, last
+FROM pioneer
+WHERE id NOT IN (
+    SELECT pioneer_id
+    FROM turing_award
+);
 
 /* 
    3. Which organizations (name only) have no associated pioneer?  
    (1 column, 1 row)
 */
 
+SELECT name
+FROM organization
+WHERE id NOT IN (
+    SELECT organization_id
+    FROM pioneer_org_xref
+);
 
 /* 
    4. For this question, you may not use IN or EXISTS.  
@@ -43,17 +62,47 @@ SET search_path = history;
       (2 columns, 1 row)
  */
 
+SELECT first, last
+FROM pioneer
+WHERE id = (
+    SELECT pioneer_id
+    FROM turing_award
+    WHERE year_awarded = 2014
+);
 
 /*
     5. Which pioneers (first, last) are associated with the organization
     "MySQL AB"? (2 columns, 3 rows)
 */
 
+SELECT first, last
+FROM pioneer
+WHERE id IN (
+    SELECT pioneer_id
+    FROM pioneer_org_xref
+    WHERE organization_id = (
+        SELECT id
+        FROM organization
+        WHERE name = 'MySQL AB'
+    )
+);
 
 /*
     6. Which pioneers (first, last) founded organizations (i.e., have "Co-founder" in their role) between 1970 and 1990? (2 columns, 3 rows)
 */
 
+SELECT first, last
+FROM pioneer
+WHERE id IN (
+    SELECT pioneer_id
+    FROM pioneer_org_xref
+    WHERE role LIKE '%Co-founder%'
+    AND organization_id IN (
+        SELECT id
+        FROM organization
+        WHERE founded BETWEEN 1970 AND 1990
+    )
+);
 
 /***************************************************************************
     GROUPING AND AGGREGATION.  You may use any other techniques (e.g. joins,
@@ -65,12 +114,20 @@ SET search_path = history;
     result to an integer.  (1 column, 1 row)
 */
 
+SELECT CAST(AVG(birth) AS INTEGER)
+FROM pioneer;
+
 
 /*
     8. How many pioneers were born in each year after 1940?  Give the birth
     year and the count, and order by birth year.  (2 columns, 6 rows)
 */
 
+SELECT birth, COUNT(*)
+FROM pioneer
+WHERE birth > 1940
+GROUP BY birth
+ORDER BY birth;
 
 /*
     9. Which pioneers are associated with more than one organization?  Give 
@@ -78,6 +135,11 @@ SET search_path = history;
     they are associated with.  (3 columns, 4 rows)
 */
 
+SELECT first, last, COUNT(*)
+FROM pioneer
+JOIN pioneer_org_xref ON pioneer.id = pioneer_id
+GROUP BY first, last
+HAVING COUNT(*) > 1;
 
 /*
     10. Which organizations are associated with more than one pioneer who are 
@@ -85,6 +147,13 @@ SET search_path = history;
     deceased pioneers. (2 columns, 1 row)
 */
 
+SELECT name, COUNT(*)
+FROM organization
+JOIN pioneer_org_xref ON organization.id = organization_id
+JOIN pioneer ON pioneer.id = pioneer_id
+WHERE death IS NOT NULL
+GROUP BY name
+HAVING COUNT(*) > 1;
 
 /***************************************************************************
     SET OPERATIONS.  While these can be answered in other ways, 
@@ -96,11 +165,39 @@ SET search_path = history;
     1930? (2 columns, 5 rows)
 */
 
+SELECT first, last
+FROM pioneer
+WHERE id IN (
+    SELECT pioneer_id
+    FROM turing_award
+)
+UNION
+SELECT first, last
+FROM pioneer
+WHERE birth < 1930;
 
 /*
     12. Which pioneers (first, last) were associated with University of California - Berkeley and won a Turing award? (2 columns, 1 row)
 */
 
+SELECT first, last
+FROM pioneer
+WHERE id IN (
+    SELECT pioneer_id
+    FROM pioneer_org_xref
+    WHERE organization_id = (
+        SELECT id
+        FROM organization
+        WHERE name = 'University of California - Berkeley'
+    )
+)
+INTERSECT
+SELECT first, last
+FROM pioneer
+WHERE id IN (
+    SELECT pioneer_id
+    FROM turing_award
+);
 
 /***************************************************************************
     CHALLENGE QUESTIONS.  You may use any techniques, including subqueries,
@@ -113,6 +210,13 @@ SET search_path = history;
     last and approximate age at death. (3 columns, 1 row)
 */
 
+SELECT first, last, death - birth AS age_at_death
+FROM pioneer
+WHERE death IS NOT NULL AND (death - birth) = (
+    SELECT MAX(death - birth)
+    FROM pioneer
+    WHERE death IS NOT NULL
+);
 
 /*
     14. Provide a list of pioneers (first, last) who won Turing awards, along 
@@ -124,5 +228,12 @@ SET search_path = history;
     same column.
 */
 
-
-
+SELECT first, last, COUNT(*), STRING_AGG(name, ', ')
+FROM pioneer
+JOIN pioneer_org_xref ON pioneer.id = pioneer_id
+JOIN organization ON organization.id = organization_id
+WHERE pioneer.id IN (
+    SELECT pioneer_id
+    FROM turing_award
+)
+GROUP BY first, last;
